5b47bf4c2fe3a0f0be5bfb3201bf9a0a6fdada06
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-09-09 09:43:45 +0530

    debug

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     // String shorthand for simple use-cases
-  //     "/api": { target: "https://netflix-backend-6kdl.onrender.com",
-  //       changeOrigin: true,
-  //       secure: false,
-  //      },
-      
-  //   },
-  // },
+  
 });
